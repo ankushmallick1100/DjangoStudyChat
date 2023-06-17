056ae3fd9724017e449a74bc88488d218e555659
Ankush Mallick <76246903+ankushmallick1100@users.noreply.github.com>
2023-06-18 01:46:20 +0530

    Changes Updated - 6

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('login/', views.loginPage, name='login'),
     path('room/<str:pk>', views.room, name="room"),
     path('create-room/',views.createRoom, name="create-room"),
     path('update-room/<str:pk>',views.updateRoom, name="update-room"),
