6af3d7f8f2456bb9f28736474dd9b8e7c3ee370c
Ankush Mallick <76246903+ankushmallick1100@users.noreply.github.com>
2023-06-19 20:57:18 +0530

    Changes Updated - 12

--- base/urls.py
+++ base/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
     path('register/', views.registerPage, name='register'),
+
     path('room/<str:pk>', views.room, name="room"),
+    path('profile/<str:pk>', views.userProfile, name='user-profile'),
+
     path('create-room/',views.createRoom, name="create-room"),
     path('update-room/<str:pk>',views.updateRoom, name="update-room"),
     path('delete-room/<str:pk>',views.deleteRoom, name="delete-room"),
