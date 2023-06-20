64798e866c60f41917e7d2550a1b78ea42045db7
Ankush Mallick <76246903+ankushmallick1100@users.noreply.github.com>
2023-06-20 23:48:35 +0530

    Changes Updated - 15

--- base/views.py
+++ base/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm,UserForm
 
 # Create your views here.
 
@@ -51,15 +51,18 @@ def logoutUser(request):
 
 
 def registerPage(request):
-    form = UserCreationForm(request.POST);
-    if form.is_valid():
-        user = form.save(commit=False)
-        user.username = user.username.lower();
-        user.save();
-        login(request,user);
-        return redirect('home');
-    else:
-        messages.error(request, "An error occured during registration");
+    form = UserCreationForm();
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST);
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower();
+            user.save();
+            login(request,user);
+            return redirect('home');
+        else:
+            messages.error(request, "An error occured during registration");
         
     context = {'form':form};
     return render(request,'base/login_register.html',context);
@@ -176,3 +179,16 @@ def deleteMessage(request,pk):
         return redirect('home');
 
     return render(request,'base/delete.html',{'obj':message});
+
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user;
+    form = UserForm(instance=user);
+
+    if request.method == 'POST':
+        form = UserForm(request.POST,instance=user);
+        if form.is_valid():
+            form.save();
+            return redirect('user-profile',pk = user.id);
+    return render(request,'base/update-user.html',{'form':form});
